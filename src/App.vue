254b9592c05d5a0a4e8bca3c2471831f67e6cc13
Romain Grente <romain.grente@gmail.com>
2021-07-23 20:24:38 +0200

    fix fido2 register authenticate

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <v-container class="fill-height" fluid>
      <v-row align="center" justify="center">
           <v-col cols="12" sm="6" md="3" align="center">
-          <v-card elevation="6" light tag="section" v-if="accessToken == null">
+          <v-card elevation="6" light tag="section" v-if="loginTokenState == null">
             <v-card-title>
               <v-layout align-center justify-space-between>
            
@@ -21,7 +21,7 @@
                       type="warning"
                       dismissible
                     >Invalid username</v-alert>
-              <v-form class="mt-5">
+              <v-form class="mt-5" v-on:submit.prevent="sendUsername">
                 <v-text-field
             label="Username"
             outlined
@@ -35,13 +35,13 @@
     >
 Register              </v-btn>
               <v-spacer></v-spacer>
-              <v-btn color="primary" :large="$vuetify.breakpoint.smAndUp" @click="sendUsername()">
+              <v-btn color="primary" :large="$vuetify.breakpoint.smAndUp" @click="sendUsername">
                 Login
               </v-btn>
             </v-card-actions>
           </v-card>
 
-          <v-card elevation="6" light tag="section" v-if="accessToken != null">
+          <v-card elevation="6" light tag="section" v-if="loginTokenState == 0">
             <v-card-title>
               <v-layout align-center justify-space-between>
            
@@ -52,28 +52,17 @@ Register              </v-btn>
             </v-card-title>
             <v-divider></v-divider>
             <v-card-text>
-              <v-alert
-                      v-model="invalidUsername"
-                      dense
-                      type="warning"
-                      dismissible
-                    >Invalid username</v-alert>
-              <v-form class="mt-5">
-                <v-text-field
-            label="Usernameeee"
-            outlined
-                              v-model="username"></v-text-field>
-              </v-form>
+             
             </v-card-text>
             <v-divider></v-divider>
             <v-card-actions :class="{ 'pa-3': $vuetify.breakpoint.smAndUp }">
                <v-btn
-      text
+      text @click="fido2register"
     >
 Register              </v-btn>
               <v-spacer></v-spacer>
-              <v-btn color="primary" :large="$vuetify.breakpoint.smAndUp" @click="sendUsername()">
-                Login
+              <v-btn color="primary" :large="$vuetify.breakpoint.smAndUp" @click="fido2authenticate">
+                Authenticate
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -86,9 +75,10 @@ Register              </v-btn>
   </v-app>
 </template>
 
-<script src="@/cbor.js"></script>
 <script>
 import axios from "axios";
+import CBOR from "@/cbor.js";
+import jwt_decode from "jwt-decode";
 
 export default {
   name: 'App',
@@ -96,7 +86,8 @@ export default {
   data: () => ({
     username: null,
     invalidUsername: false,
-    accessToken: null
+    loginToken: null,
+    loginTokenState: null
   }),
 
   methods: {
@@ -110,9 +101,9 @@ export default {
       })
         .then(response => {
           if (response.status == 200) {
-            this.accessToken = response.data.access_token
+            this.loginToken = response.data.access_token
             this.invalidUsername = false
-            this.fido2register()
+            this.loginTokenState = jwt_decode(this.loginToken).state
           }
 
         })
@@ -124,22 +115,25 @@ export default {
         });
     },
     fido2register() {
+      var token = this.loginToken
       fetch('https://localhost:5000/api/register/fido2/begin', {
       method: 'POST',
-      headers: {'Authorization': 'Bearer ' + this.accessToken},
+      headers: {'Authorization': 'Bearer ' + token},
+      credentials: 'include'
     }).then(function(response) {
       if(response.ok) return response.arrayBuffer();
       throw new Error('Error getting registration data!');
     }).then(CBOR.decode).then(function(options) {
       return navigator.credentials.create(options);
     }).then(function(attestation) {
-      return fetch('/api/register/fido2/complete', {
+      return fetch('https://localhost:5000/api/register/fido2/complete', {
         method: 'POST',
-        headers: {'Content-Type': 'application/cbor', 'Authorization': 'Bearer ' + this.accessToken},
+        headers: {'Content-Type': 'application/cbor', 'Authorization': 'Bearer ' + token},
         body: CBOR.encode({
           "attestationObject": new Uint8Array(attestation.response.attestationObject),
           "clientDataJSON": new Uint8Array(attestation.response.clientDataJSON),
-        })
+        }),
+        credentials: 'include'
       });
     }).then(function(response) {
       var stat = response.ok ? 'successful' : 'unsuccessful';
@@ -149,7 +143,48 @@ export default {
     }).then(function() {
       window.location = '/';
     });
-    }
+  },
+  fido2authenticate() {
+    var token = this.loginToken
+    var vm = this;
+    axios({
+      url: 'https://localhost:5000/api/authenticate/fido2/begin',
+      method: 'post',
+      headers: {'Authorization': 'Bearer ' + vm.loginToken},
+      withCredentials: true,
+      responseType: 'arraybuffer'
+    }).then(response => {
+      if(response.status == 200) return response.data;
+      throw new Error('No credential available to authenticate!');
+    }).then(CBOR.decode).then(function(options) {
+      return navigator.credentials.get(options);
+    }).then(function(assertion) {
+      return axios({
+        url: 'https://localhost:5000/api/authenticate/fido2/complete',
+        method: 'post',
+        headers: {'Content-Type': 'application/cbor', 'Authorization': 'Bearer ' + vm.loginToken},
+        data: CBOR.encode({
+          "credentialId": new Uint8Array(assertion.rawId),
+          "authenticatorData": new Uint8Array(assertion.response.authenticatorData),
+          "clientDataJSON": new Uint8Array(assertion.response.clientDataJSON),
+          "signature": new Uint8Array(assertion.response.signature)
+        }),
+        withCredentials: true
+      })
+    }).then(function(response) {
+      if(response.status == 200) {
+        var stat = 'successful';
+        vm.loginToken = response.data.access_token;
+        vm.loginTokenState = jwt_decode(vm.loginToken);
+      }
+      else {
+        var stat = 'unsuccessful';
+      }
+        alert('Authentication ' + stat + ' More details in server log...');
+    }, function(reason) {
+      alert(reason);
+    });
+  }
   }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080, // CHANGE YOUR PORT HERE!
+    https: true,
+    hotOnly: false,
+  },
 }
