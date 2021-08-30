866c90d281ecbedd1648e7a24bb4736374b48f8f
Romain Grente <romain.grente@gmail.com>
2021-08-30 18:57:02 +0000

    change port and disable https

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   devServer: {
     host: '0.0.0.0',
-    port: 8080, // CHANGE YOUR PORT HERE!
-    https: true,
+    port: 20000, // CHANGE YOUR PORT HERE!
+    https: false,
     hotOnly: false,
     disableHostCheck: true
   }
